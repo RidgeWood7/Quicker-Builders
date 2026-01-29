7467f82d99a874bad17f45d56707b3f87a7411e0
MUKILTEO\1725631 <1725631@mukilteo.wednet.edu>
2026-01-29 13:27:51 -0800

    Worked on coin

---
+++ Assets/Valerie/Vals Scripts/Coin.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private bool _isAttatched = false;
+    private bool _playerDead = false;
+    private Transform _targetTransform;
+    [SerializeField] private Transform _coin;
+
+    void Update()
+    {
+        if (_playerDead == true && _isAttatched == true)
+        {
+            _isAttatched = false;
+            _targetTransform = null;
+        }
+
+        if (_isAttatched == true)
+        {
+            _coin.position = Vector2.Lerp(_coin.position, _targetTransform.position, Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.gameObject);
+        if (collision.gameObject.CompareTag("Player") && _playerDead == false && _isAttatched == false)
+        {
+            _isAttatched = true;
+            _targetTransform = collision.gameObject.transform;
+        }
+    }
+}
