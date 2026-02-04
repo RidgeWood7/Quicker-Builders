b76491e192e2207a4d9299f1ce6463c43c3f9d70
MUKILTEO\1726377 <chasebannavong@gmail.com>
2026-02-04 13:33:51 -0800

    added leaderboard ui its cool and grid stuff

--- Assets/Mikel/Scripts/PlaceAbleObject.cs
+++ Assets/Mikel/Scripts/PlaceAbleObject.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 public class PlaceAbleObject : MonoBehaviour
 {
     public bool isGrabbed;
+    public Vector2 position;
+    public bool isMouseControlled;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,16 +15,31 @@ public class PlaceAbleObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-          isGrabbed = false;
-        }
-        if (isGrabbed)
+        if (isGrabbed&& isMouseControlled)
         {
             Vector3 mousePosition = Mouse.current.position.ReadValue();
             mousePosition.z = 10f; // Set this to be the distance from the camera to the object
             Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            worldPosition.x=Mathf.Round(worldPosition.x);
+            worldPosition.y=Mathf.Round(worldPosition.y);
+
             transform.position = worldPosition;
         }
+    }
+
+    public void PlacedDown(InputAction.CallbackContext ctx)
+    {
+         isGrabbed = true;
+    }
+
+    public void Move()
+    {
+        Vector3 mousePosition = Mouse.current.position.ReadValue();
+        mousePosition.z = 10f; // Set this to be the distance from the camera to the object
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        worldPosition.x = Mathf.Round(worldPosition.x);
+        worldPosition.y = Mathf.Round(worldPosition.y);
+
+        transform.position = worldPosition;
     }
 }
