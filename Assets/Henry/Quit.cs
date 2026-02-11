8078cc2baee9f8b5d8784a30f84e8cf616ee91d9
MUKILTEO\1602247 <abel4prezz@gmail.com>
2026-02-11 11:44:19 -0800

    Fixed Quit Button

---
+++ Assets/Henry/Quit.cs
@@ -0,0 +1,14 @@
+using UnityEditor;
+using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+
+    #if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+    #endif
+    }
+}
